a6dd8c6f0fd7945cdf4569c955afaa9898ebbefb
Ermias <ermiastam@gmail.com>
2018-06-11 15:00:39 -0700

    initial commit

---
+++ src/main/java/com/ermias/gateway/ApiGatewayServiceApplication.java
@@ -0,0 +1,23 @@
+package com.ermias.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@EnableZuulProxy
+@SpringBootApplication
+//@EnableDiscoveryClient
+public class ApiGatewayServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayServiceApplication.class, args);
+	}
+
+
+
+
+
+}
